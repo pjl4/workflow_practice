3a639a32cbeb7f43b02fc551dd0d2d4ab9e1e8ba
SRBond <skyler.bond7@gmail.com>
2020-02-12 08:53:46 -0800

    commented

--- workflow_practice/src/App.js
+++ workflow_practice/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import './App.css';
-import Hello from './Hello';
+import React from "react";
+import "./App.css";
+import Hello from "./Hello";
 
 //what a great function
+//wow yeah I agree 10/10
 function App() {
 	return (
 		<div className="App">
